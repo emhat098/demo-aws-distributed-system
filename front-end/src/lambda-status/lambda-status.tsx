837fa763ffdf715f6ea6166b489422ee59a19204
Em Ha Tuan <emhat098@gmail.com>
2024-12-27 17:03:12 +0700

    Update JWT Authorizer in AWS Lambda.

--- front-end/src/lambda-status/lambda-status.tsx
+++ front-end/src/lambda-status/lambda-status.tsx
@@ -1,19 +1,33 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button, Tag } from "antd";
 import { useEffect, useState } from "react";
 
 type StatusType = 'loading' | 'success' | 'failed'
 
 const LambdaStatus = () => {
-    const [status, setStatus] = useState<StatusType>('loading');
+    const [status, setStatus] = useState<StatusType>('loading')
+    const { getAccessTokenSilently } = useAuth0();
 
     useEffect(() => {
-        fetch(import.meta.env.VITE_BASE_API_URL + '/helloworld')
-            .then(res => res.ok  && setStatus('success'))
-            .catch(err => {
-                setStatus('failed')
-                console.log(err);
+        getAccessTokenSilently().then(token => {
+            fetch(import.meta.env.VITE_BASE_API_URL + '/helloworld', {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
             })
-    }, []);
+                .then(res => {
+                    if (res.ok) {
+                        setStatus('success')
+                    }
+                })
+                .catch(err => {
+                    setStatus('failed')
+                    console.log(err);
+                })
+        })
+
+    }, [getAccessTokenSilently]);
 
     if (status === 'loading') {
         return (
